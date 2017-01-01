b1db8f6de254bf7670690bd72980e64a3f7685de
Erich Abellera <ErichAbellera@Erich-Abelleras-MacBook-Pro.local>
2016-12-31 22:33:32 -0800

    Add actions and action tests

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,48 @@
+var expect = require('expect');
+var actions = require('actions');
+
+describe('Actions', () => {
+  it('should exist', () => {
+    expect(actions).toExist();
+  });
+
+  it('should generate search text action', () => {
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText: 'Some search text'
+    };
+
+    var res = actions.setSearchText(action.searchText);
+    expect(res).toEqual(action);
+  });
+
+  it('should generate add todo action', () => {
+    var action = {
+      type: 'ADD_TODO',
+      text: 'New item'
+    };
+
+    var res = actions.addToDo(action.text);
+    expect(res).toEqual(action);
+  });
+
+  it('should generate toggle show completed action', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+
+    var res = actions.toggleShowCompleted();
+    expect(res).toEqual(action);
+  });
+
+  it('should generate toggle to do action', () => {
+    var action = {
+      type: 'TOGGLE_TODO',
+      id: 20
+    };
+
+    var res = actions.toggleToDo(action.id);
+    expect(res).toEqual(action);
+  });
+
+});
